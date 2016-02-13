e6bf773472c407e35c586afb8ce9455074f4d5b2
Alyssa Hislop <alyssa.l.hislop@gmail.com>
2016-02-12 22:53:11 -0800

    updated
    Fixed list append

--- counterjson.py
+++ counterjson.py
@@ -7,13 +7,13 @@ import json
 #Replace 'foo.json' with the json file that you want to use
 
 CREATED_AT_FMT='%a %b %d %H:%M:%S %z %Y'
-document = 'data/tweets-0001.json'
+document = 'foo.json'
 aList = []
 
-#Test to see what the 'created_at' timestamp will look like
+#Store these created_at elements as a list
 for row in open(document):
     a = json.loads(row)
-    print(a['created_at'])
+    aList.append(a['created_at'])
 
 #Function for removing the time information from the timestamp
 def date_from_created(created):
@@ -23,7 +23,9 @@ def date_from_created(created):
 
 #This counts the things
 date_count=Counter(
-    date_from_created(row['created_at']) 
+    date_from_created(row) 
     for row in aList
 )
+
+print(date_count)
 print(aList)
